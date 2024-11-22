9f7a4339eeddd7f4fba983d8b24a3df3e15c7e9f
soniprem831 <premsoni8838@gmail.com>
2024-11-22 23:39:23 +0530

    second commit

--- admin/vite-project/src/App.jsx
+++ admin/vite-project/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url= "http://localhost:4000";
+  const url= "https://quickbite-ljob.onrender.com";
   console.log(url)
   return (
     <div>
